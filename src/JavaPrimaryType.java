e3bcdabc99e26c60952fd3dc07d4edb784e1c2a5
Charles An <mucollabo@gmail.com>
2020-05-06 16:27:48 +0900

    study until ex_16

---
+++ src/JavaPrimaryType.java
@@ -0,0 +1,10 @@
+public class JavaPrimaryType {
+    public static void main(String[] args) {
+        boolean isL = false;
+        char suit = 'H';
+        int year = 2016;
+        double latitude = 37.52127220511242;
+        double longitude = 127.0074462890625;
+        System.out.println(latitude + "\t" + longitude);
+    }
+}
