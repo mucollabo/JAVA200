bf524ff1566a79d0c04baa6eaeb1e71ccf7ec00f
Charles An <mucollabo@gmail.com>
2020-05-16 12:52:24 +0900

    study until ex_19

---
+++ src/JavaTypeConversion.java
@@ -0,0 +1,9 @@
+public class JavaTypeConversion {
+    public static void main(String[] args) {
+        String slat = "37.52127220511242   ";
+        //String slat = "hello";
+        //공백 제거 후 double로 타입 변환
+        double latitude = Double.parseDouble(slat.trim());
+        System.out.println(latitude);
+    }
+}
