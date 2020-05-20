9307a2bfa542ed0cb8c007998db3cc370526b063
Charles An <mucollabo@gmail.com>
2020-05-20 15:45:52 +0900

    study until ex_029

---
+++ src/s024Hello.java
@@ -0,0 +1,14 @@
+public class s024Hello
+{
+    public static void showHelp()
+    {
+        System.out.println("**************************************");
+        System.out.println("*   이 프로그래밍은 Java200이 만들었습니다.   *");
+        System.out.println("**************************************");
+    }
+
+    public static void main(String[] args)
+    {
+        showHelp();
+    }
+}
